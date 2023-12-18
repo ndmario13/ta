96e891ca2bbf4bba6661ca7ab5af6bf228593266
ndmario13 <150593178+ndmario13@users.noreply.github.com>
2023-12-18 09:26:39 +0100

    add files via upload

---
+++ caeser boutique/caesar.js
@@ -0,0 +1,9 @@
+let hom = document.getElementById("hom").onclick
+function(){
+    if(document.style.display='')
+    document.style.display="block"
+else{
+    
+}
+
+}
---
+++ newp.js
@@ -0,0 +1,32 @@
+
+
+// defining variable for the login 
+let account_Login = document.querySelector(".accountLog");
+let LogIn = document.getElementById("LOGIN");
+let subBtn = document.querySelector("SubmitBtn");
+var formContainerCreate = document.querySelector(".formCreateContainer");
+var loginBtn = document.getElementById("signin");
+// var _forgotpword = document.getElementById("ForgotPassword")
+LogIn.addEventListener("click", function(){
+    if (account_Login.style.display = "none") {
+        account_Login.style.display = "block"
+        formContainerCreate.style.display = "none"
+        // console.log(formContainerCreate)
+        LogIn.style.display = "none" 
+        // loginBtn.style.display =""
+        // loginBtn.innerText.valueOf = 0;
+    //  _forgotpword.style.display = "block"
+    subBtn.value == account_Login.value
+    alert("acess to caesar boutique")
+
+
+
+
+
+
+
+    }
+  
+
+    
+})
